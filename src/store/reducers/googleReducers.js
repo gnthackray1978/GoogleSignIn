2db039905ae4a3061d683bc3f42a1937221b3d6a
George <gnthackray@btinternet.com>
2019-12-13 21:36:41 +0000

    dependency fixs

--- src/store/reducers/googleReducers.js
+++ src/store/reducers/googleReducers.js
@@ -1,8 +1,8 @@
 
 export default (state = {
   GoogleApiParams : {
-    scriptId : "MQ9uI5jQzqKm4wt01EV3l5pIG0z7T6jhI",
-    clientId : '183174195107-spa00qp12u40nj4kb8od7nudc149l74q.apps.googleusercontent.com',
+    scriptId : "",
+    clientId : '',
     scopes : 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/spreadsheets',
     cookie_policy: 'single_host_origin',
     login_hint: 'login hint',
